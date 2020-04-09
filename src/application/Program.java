74f03b2a59f22a0aa9759f25d5bc166ce412ae49
Jose Neto <josenetopr5@gmail.com>
2020-04-09 14:38:52 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,25 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
-import model.entities.Department;
+//import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-    public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    public static void main(String[] args)  {
+        //SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         
-        Department obj = new Department(1, "Books");
+        //Department obj = new Department(1, "Books");
         
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new java.sql.Date(sdf.parse("15/06/1998").getTime()), 3000.0, obj);
+        //Seller seller = new Seller(21, "Bob", "bob@gmail.com", new java.sql.Date(sdf.parse("15/06/1998").getTime()), 3000.0, obj);
         
         SellerDAO sellerDao = DaoFactory.createSellerDao();
+        
+        Seller seller = sellerDao.findById(3);
         
         System.out.println(seller);
     }
